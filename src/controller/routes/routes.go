5cd3726d9937570a6458dddc87834e23fc24b21e
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2023-09-16 11:35:07 +0000

    Implemetation User controller

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joaomauriciodev/crud-go/src/controller"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+	r.GET("/user/:id", controller.FindUserById)
+	r.GET("/user/:email", controller.FindUserByEmail)
+	r.POST("/user", controller.CreateUser)
+	r.PUT("/user", controller.UpdateUser)
+	r.DELETE("/user", controller.DeleteUser)
+}
---
+++ src/controller/userController.go
@@ -0,0 +1,13 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func FindUserById(c *gin.Context) {}
+
+func FindUserByEmail(c *gin.Context) {}
+
+func CreateUser(c *gin.Context) {}
+
+func UpdateUser(c *gin.Context) {}
+
+func DeleteUser(c *gin.Context) {}
