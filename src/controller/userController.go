7cc236b2f49aa7c048d138f769c607c57e866c3c
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2023-09-18 12:11:07 +0000

    implemented user response and request.

--- src/controller/userController.go
+++ src/controller/userController.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	resterr "github.com/joaomauriciodev/crud-go/src/configuration/rest_err"
+	"github.com/joaomauriciodev/crud-go/src/controller/model/request"
 )
 
 func FindUserById(c *gin.Context) {}
@@ -10,8 +14,17 @@ func FindUserById(c *gin.Context) {}
 func FindUserByEmail(c *gin.Context) {}
 
 func CreateUser(c *gin.Context) {
-	err := resterr.NewBadRequestError("Você chamou a rota errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := resterr.NewBadRequestError(fmt.Sprintf("There are some incorret fields, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+	c.JSON(http.StatusOK, userRequest)
+
 }
 
 func UpdateUser(c *gin.Context) {}
