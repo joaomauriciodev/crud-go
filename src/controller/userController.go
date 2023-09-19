e88bad64c17215168ffd8f77ff09afa932b761b2
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2023-09-19 11:54:49 +0000

    Implemented logger

--- src/controller/userController.go
+++ src/controller/userController.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joaomauriciodev/crud-go/src/configuration/logger"
 	"github.com/joaomauriciodev/crud-go/src/configuration/validation"
 	"github.com/joaomauriciodev/crud-go/src/controller/model/request"
+	"go.uber.org/zap"
 )
 
 func FindUserById(c *gin.Context) {}
@@ -14,10 +16,17 @@ func FindUserById(c *gin.Context) {}
 func FindUserByEmail(c *gin.Context) {}
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+		zap.String("methodHttp", "POST"),
+	)
+
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		restErr := validation.ValidateUserError(err)
+
+		logger.Error("Error trying to validate user info", err)
 
 		c.JSON(restErr.Code, restErr)
 		return
